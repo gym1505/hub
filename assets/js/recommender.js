21424087bddd8b3dd6dde0a15805268118f331c3
Alex Maksimov <maleksware@gmail.com>
2023-05-03 20:01:20 +1000

    Fix adaptivity issues on smartphones and tablets

--- assets/js/recommender.js
+++ assets/js/recommender.js
@@ -13,11 +13,7 @@ var estimated_rating = 0;
 // Vars for charts
 var tags = {};
 google.charts.load('current', { 'packages': ['corechart', 'calendar'] });
-var titleTextStyle = {
-    fontSize: 18,
-    color: '#393939',
-    bold: false
-};
+
 var colors = ['#f44336', '#E91E63', '#9C27B0', '#673AB7', '#2196F3', '#009688',
     '#8BC34A', '#CDDC39', '#FFC107', '#FF9800', '#FF5722', '#795548', '#607D8B', '#E65100',
     '#827717', '#004D40', '#1A237E', '#6200EA', '#3F51B5', '#F50057', '#304FFE', '#b71c1c'];
@@ -475,16 +471,15 @@ function drawCharts() {
     tags.addColumn('number', 'solved');
     tags.addRows(tagTable);
     var tagOptions = {
-        width: Math.max(600, $('#tags').width()),
-        height: Math.max(600, $('#tags').width()) * 0.75,
-        chartArea: { width: '80%', height: '100%' },
-        title: 'Tags of ' + handle,
+        width: $('#tags').width(),
+        height: $('#tags').height(),
+        chartArea: { width: '100%', height: '100%' },
         pieSliceText: 'none',
         legend: {
             position: 'right',
             alignment: 'center',
             textStyle: {
-                fontSize: 12,
+                fontSize: 10,
                 fontName: 'Nunito',
                 color: '#bbbbbb'
             }
@@ -496,7 +491,6 @@ function drawCharts() {
         fontName: 'Nunito',
         backgroundColor: '#292929',
         color: '#bbbbbb',
-        titleTextStyle: titleTextStyle,
         colors: colors.slice(0, Math.min(colors.length, tags.getNumberOfRows())),
     };
     var tagChart = new google.visualization.PieChart(document.getElementById('tags'));
